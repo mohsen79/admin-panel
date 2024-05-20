db339948ee58daa709233da697a8d231e271ab2c
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-05-20 19:17:47 +0330

    issues page created & active link styles & classNames package installed

--- app/NavBar.tsx
+++ app/NavBar.tsx
@@ -1,19 +1,27 @@
+'use client';
+
 import Link from "next/link";
 import { FC } from "react";
 import { AiFillBug } from 'react-icons/ai';
+import { usePathname } from "next/navigation";
+import classNames from "classnames";
 
 const NavBar: FC = () => {
+    const path = usePathname();
     const links = [
-        { label: 'Dashboard', href: '/dashboard' },
+        { label: 'Dashboard', href: '/' },
         { label: 'Issuses', href: '/issues' },
     ];
 
     return (
         <nav className="flex space-x-5 px-3 h-10 border-b items-center mb-3">
             <Link href="/"><AiFillBug /></Link>
-            <ul className="flex space-x-3 text-zinc-100">
+            <ul className="flex space-x-3 text-zinc-500">
                 {links.map(link => <li><Link
-                    className="hover:text-zinc-300"
+                    className={classNames({
+                        'text-zinc-100': path === link.href,
+                        'hover:text-zinc-300': true
+                    })}
                     key={link.href}
                     href={link.href}>{link.label}</Link></li>)}
             </ul>
