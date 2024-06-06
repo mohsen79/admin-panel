d129e69326974489c1f6db6e2534b7c3c84ff7ec
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-06-06 21:40:27 +0330

    react hook form installed & set form with react hook form

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -4,14 +4,37 @@ import React from 'react';
 import { Button, TextField } from '@radix-ui/themes';
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
+import { useForm, Controller } from 'react-hook-form';
+import { useRouter } from 'next/navigation';
+
+interface IssueForm {
+    title: string
+    description: string
+}
 
 const NewIssue = () => {
+    const { register, control, handleSubmit } = useForm<IssueForm>();
+    const router = useRouter();
+
+    const submitForm = async (data: object) => {
+        await fetch('/api/issues', {
+            method: 'POST',
+            body: JSON.stringify(data)
+        });
+
+        router.push('/issues');
+    }
+
     return (
-        <div className='max-w-xl space-y-2 p-3'>
-            <TextField.Root placeholder="new issue" />
-            <SimpleMDE placeholder='description' />
+        <form className='max-w-xl space-y-2 p-3' onSubmit={handleSubmit((data) => submitForm(data))}>
+            <TextField.Root placeholder="new issue" {...register('title')} />
+            <Controller
+                name='description'
+                control={control}
+                render={({ field }) => <SimpleMDE placeholder='description' {...field} />}
+            />
             <Button>Add</Button>
-        </div>
+        </form>
     )
 }
 
