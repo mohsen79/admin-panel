911ad67268e1b16b2ad8f6c204f295619999fd2f
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-07-10 20:23:45 +0330

    radix UI icons installed & create edit issue button with responsive styles

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,9 +1,11 @@
 import { IssueStatusBadge } from '@/app/components'
 import prisma from '@/prisma/client'
-import { Card, Flex, Heading } from '@radix-ui/themes'
+import { Box, Button, Card, Flex, Grid, Heading } from '@radix-ui/themes'
+import Link from 'next/link'
 import { notFound } from 'next/navigation'
 import React from 'react'
 import Markdown from 'react-markdown'
+import { Pencil2Icon } from '@radix-ui/react-icons';
 
 interface Props {
     params: { id: string }
@@ -15,25 +17,27 @@ const IssueDetial = async ({ params }: Props) => {
     if (!issue) notFound();
 
     return (
-<<<<<<< HEAD
-        <div className='mx-5'>
-            <Heading>{issue.title}</Heading>
-            <Flex className='space-x-3' my="3">
-                <IssueStatusBadge status={issue.status} />
-                <p>{issue.createdAt.toDateString()}</p>
-            </Flex>
-            <Card className='prose'>
-                <Markdown>{issue.description}</Markdown>
-            </Card>
-=======
-        <div>
-                <p>{issue.title}</p>
-                <p>{issue.description}</p>
-                <p>{issue.status}</p>
-                <p>{issue.createdAt.toDateString()}</p>
->>>>>>> 51eaa30f3f0d32e015a740efe67681354b3a9bc8
-            </div>
-            )
+        <Grid className='mx-5' columns={{ initial: '1', md: '2' }} gap="3">
+            <Box>
+                <Heading>{issue.title}</Heading>
+                <Flex className='space-x-3' my="3">
+                    <IssueStatusBadge status={issue.status} />
+                    <p>{issue.createdAt.toDateString()}</p>
+                </Flex>
+                <Card className='prose'>
+                    <Markdown>{issue.description}</Markdown>
+                </Card>
+            </Box>
+            <Box>
+                <Button>
+                    <Pencil2Icon />
+                    <Link href="">
+                        Edit Issue
+                    </Link>
+                </Button>
+            </Box>
+        </Grid>
+    )
 }
 
-            export default IssueDetial
+export default IssueDetial
