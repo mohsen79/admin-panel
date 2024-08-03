e20d80ab5d3496045a916a6c7b75e4a9b2b3ac7b
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-08-03 21:10:01 +0330

    new validation shcema created for assigned issue and used in issues api

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,5 +1,5 @@
 import { authOptions } from "@/app/auth/authOptions";
-import { IssueSchema } from "@/app/schemas/validationSchemas";
+import { pathIssueSchema } from "@/app/schemas/validationSchemas";
 import prisma from "@/prisma/client";
 import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
@@ -9,9 +9,16 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     if (!session) return NextResponse.json({}, { status: 401 });
 
     const body = await request.json();
-    const validation = IssueSchema.safeParse(body);
+    const validation = pathIssueSchema.safeParse(body);
+    const { title, description, assignedToUserId } = body;
 
     if (!validation.success) return NextResponse.json(validation.error.errors, { status: 400 })
+
+    if (assignedToUserId) {
+        const user = await prisma.user.findUnique({ where: { id: assignedToUserId } });
+
+        if (!user) return NextResponse.json('invalid user', { status: 404 });
+    }
 
     const issue = await prisma.issue.findUnique({ where: { id: parseInt(params.id) } });
 
@@ -20,8 +27,9 @@ export async function PATCH(request: NextRequest, { params }: { params: { id: st
     const updatedIssue = await prisma.issue.update({
         where: { id: issue.id },
         data: {
-            title: body.title,
-            description: body.description
+            title,
+            description,
+            assignedToUserId
         }
     });
 
--- app/schemas/validationSchemas.ts
+++ app/schemas/validationSchemas.ts
@@ -4,3 +4,9 @@ export const IssueSchema = z.object({
     title: z.string().min(1, 'title is required').max(100),
     description: z.string().min(1, 'description is requried')
 });
+
+export const pathIssueSchema = z.object({
+    title: z.string().min(1, 'title is required').max(100).optional(),
+    description: z.string().min(1, 'description is requried').optional(),
+    assignedToUserId: z.string().min(1, 'AssignedToUserId is required').max(255).optional().nullable()
+});
