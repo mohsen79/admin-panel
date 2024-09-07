f14ea3a2afc61ec5e6b654fc0a78dbfc69234a90
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-09-07 20:09:32 +0330

    show latest five issues in home page

---
+++ app/LatestIssues.tsx
@@ -0,0 +1,37 @@
+import prisma from '@/prisma/client'
+import { Avatar, Card, Flex, Table } from '@radix-ui/themes'
+import React from 'react'
+import { IssueStatusBadge } from './components'
+import Link from 'next/link'
+
+const LatestIssues = async () => {
+    const issues = await prisma.issue.findMany({
+        orderBy: { createdAt: 'desc' },
+        take: 5,
+        include: {
+            assignedToUser: true
+        }
+    })
+    return (
+        <Card m='3'>
+            <Table.Root>
+                <Table.Body>
+                    {issues.map(issue => <Table.Row key={issue.id}>
+                        <Table.Cell>
+                            <Flex justify='between'>
+                                <Flex direction='column' gap='2'>
+                                    <Link href={`issues/${issue.id}`}>{issue.title}</Link>
+                                    <IssueStatusBadge status={issue.status} />
+                                </Flex>
+                                {issue.assignedToUser &&
+                                    <Avatar radius='full' src={issue.assignedToUser.image} fallback='?' />}
+                            </Flex>
+                        </Table.Cell>
+                    </Table.Row>)}
+                </Table.Body>
+            </Table.Root>
+        </Card>
+    )
+}
+
+export default LatestIssues
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
-import Pagination from './components/Pagination'
+import LatestIssues from './LatestIssues'
 
-export default function Home({ searchParams }: { searchParams: { page: string } }) {
-  return <Pagination currentPage={parseInt(searchParams.page) || 1} itemCount={100} pageSize={10} />
+export default function Home() {
+  return <LatestIssues />
 }
