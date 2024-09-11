5904fffa52fd5bce497f0672ff09f7d8661c284b
mohsen79 <mohsenjalali79.mj@gmail.com>
2024-09-11 19:40:48 +0330

    recharts install & IssueChart added to home page beside IssueSummary and LatesIssues

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,27 @@
 import prisma from '@/prisma/client'
 import IssuesSummary from './IssuesSummary'
 import LatestIssues from './LatestIssues'
+import { Flex, Grid } from '@radix-ui/themes';
+import IssueChart from './IssueChart';
 
 export default async function Home() {
   const open = await prisma.issue.count({ where: { status: 'OPEN' } });
   const inProgress = await prisma.issue.count({ where: { status: 'IN_PROGRESS' } });
   const closed = await prisma.issue.count({ where: { status: 'CLOSED' } });
 
-  return <>
+  return <Grid columns={{ initial: '1', md: '2' }} gap="5">
+    <Flex direction="column" gap="5">
+      <IssuesSummary
+        open={open}
+        inProgress={inProgress}
+        closed={closed}
+      />
+      <IssueChart
+        open={open}
+        inProgress={inProgress}
+        closed={closed}
+      />
+    </Flex>
     <LatestIssues />
-    <IssuesSummary open={open} inProgress={inProgress} closed={closed} />
-  </>
+  </Grid>
 }
